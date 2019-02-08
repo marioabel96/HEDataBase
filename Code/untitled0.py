4bea767b41d85aa71652ad28fe992f517e3c5061
Unknown <marioabel96@gmail.com>
2019-02-08 17:28:35 -0600

    Version 1

--- Code/untitled0.py
+++ Code/untitled0.py
@@ -5,5 +5,12 @@ Created on Fri Feb  8 14:54:26 2019
 @author: Mario Abel García
 """
 
-from ftplib import ftp
-#%%
+from ftplib import FTP
+import pandas as pd
+import numpy as np
+#%% Inventario de CT conexion via FTP
+
+ftp = FTP('216.70.82.104')
+ftp.login(user='GDL2536', passwd = 'gdl2536')
+
+#ftp.cwd('/whyfix/')
